4a1eeca295a0d883a4136fd1a7d7fa8410ad043d
palgooal <palgooal@gmail.com>
2025-08-12 21:43:23 +0300

    up

--- src/core/helpers/query-string.js
+++
@@ -1,4 +0,0 @@
-export default function createQueryString(page, limit, q) {
-  const queryString = ["?", "page", "=", page, "&", "limit", "=", limit, "&", "q", "=", q].join("")
-  return queryString
-}
